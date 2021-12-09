d6474ae7f371f872ecb2c7400a7bad6573aa1d02
Davion <daviongarcia@gmail.com>
2021-12-08 19:57:43 -0800

    asgmt 2-3

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
 
 function priority() {
-  if(high) {
+  if (pri) {
     priIcon.removeClass('far').addClass('fas').css('color', 'red')
-    high = false
-  } 
+    pri = false
+  }
   else {
     priIcon.removeClass('fas').addClass('far').css('color', 'white')
-    high = true
+    pri = true
+  }
+}
+
+class Task {
+  constructor(pri, title, desc, dd, status, userColor, category) {
+    this.pri = pri;
+    this.title = title;
+    this.desc = desc;
+    this.dd = dd;
+    this.status = status;
+    this.userColor = userColor;
+    this.category = category;
   }
 }
 
 function added() {
-  addBtn.css('background-color', 'green')
+  addBtn.css('color', 'green')
   setTimeout(() => {
-    addBtn.css('background-color', 'transparent')
+    addBtn.css('color', 'white')
   }, 2000);
+
+  let title = $('#title').val()
+  let desc = $('#desc').val()
+  let dd = $('#dd').val()
+  let status = $('#status').val()
+  let userColor = $('#userColor').val()
+  let category = $('#category').val()
+
+  newTask = new Task(pri, title, desc, dd, status, userColor, category)
+  console.log(newTask)
+  renderTask(newTask)
+  clearForm()
+}
+
+function renderTask(task) {
+  $('.taskList').append(
+    `<div class="task-card">
+    <i class="far fa-star "></i>
+
+    <div class="info">
+    <h3>Title: ${task.title}</h3>
+    <p>Description: ${task.desc}</P>    
+    </div>
+
+    <div class="details">
+    <label>Status: ${task.status}</label>
+    <label>Category: ${task.category}</label>
+    </div>
+    </div>`
+  )
+}
+
+function clearForm() {
+  $('#title').val('')
+  $('#desc').val('')
+  $('#dd').val('')
+  $('#status').val('')
+  $('#userColor').val('')
+  $('#category').val('')
 }
 
 function hideForm() {
-  if(hide) {
+  if (hide) {
     hideBtn.removeClass('fa-eye').addClass('fa-eye-slash')
     formSect.show()
     hide = false
-  } 
+  }
   else {
     hideBtn.removeClass('fa-eye-slash').addClass('fa-eye')
     formSect.hide()
@@ -31,7 +82,7 @@ function hideForm() {
 }
 
 function init() {
-  high = false
+  pri = false
   hide = false
 
   priIcon = $('#priority')
@@ -43,7 +94,6 @@ function init() {
   formSect = $('.capture')
   hideBtn = $('.fa-eye-slash')
   hideBtn.click(hideForm)
-
 }
 
 window.onload = init;
